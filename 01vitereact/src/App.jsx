900a22f918f3bf3cb070ba191447508366280c16
rajesh@cloudmojo.tech <rajesh@cloudmojo.tech>
2026-04-29 15:50:00 +0530

    added custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Test from "./Test"
 
 function App() {
-
+const username= "vismay rathore"
   return (
     <div>
 <Test/>
 <h1> testing the h1</h1>
+<p>{username}</p>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus earum dolorum?</p>
 </div>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,44 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+ 
+function MyApp(){
+  return (
+    <div>
+      <h1> Custom App!</h1>
+    </div>
+  )
+}
+// const ReactElement= {
+//     type:'a',
+//     props:{
+//         href:'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to vist google'
+// };
+const anotherUser= "vismay rathore";
+
+const reactElement= React.createElement(
+  'a',
+  {href:'https://google.com',target: '_blank'},
+  'click me to visit google',
+  anotherUser
+);
+
+const anotherElement= 
+(
+  <a href="https://google.com" target='_blank'>
+    visit google
+  </a>
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+    // anotherElement
+    reactElement
+    // <App/> 
+  // </StrictMode>,
 )
